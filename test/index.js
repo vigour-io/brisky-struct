2ab0c130e71c146ae33038caa6cc4c71edd76fdb
Jim de Beer <jim@vigour.io>
2016-10-22 14:27:13 +0200

    added start of emitters

--- test/index.js
+++ test/index.js
@@ -1,1 +1,21 @@
 'use strict'
+const { create, set, get, struct } = require('../')
+const bstamp = require('brisky-stamp')
+
+const a = create(struct, {
+  on: {
+    data: {
+      a: () => console.log('yes fire')
+    }
+  }
+})
+
+// 1 mil sets 60ms
+console.log('go set!')
+const s = bstamp.create()
+set(a, 'bla', 'x', s)
+bstamp.close(s)
+
+// console.log(a.on.data.inherits.child)
+
+// removing it
