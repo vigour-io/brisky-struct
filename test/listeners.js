3577f7ae02ae96c0eed094b04fa707526dd0e345
Jim de Beer <jim@vigour.io>
2016-10-28 19:28:28 +0200

    fixed double inheritance when branching in an object in listeners

--- test/listeners.js
+++ test/listeners.js
@@ -22,7 +22,13 @@ set(a, null, 'x')
 console.log('--------------')
 const a2 = create(a, {
   on: {
-    data: { b: () => { console.log('my instance a2') } }
+    data: { A2: () => { console.log('my instance a2') } }
+  }
+})
+
+const a3 = create(a2, {
+  on: {
+    data: { A3: () => { console.log('my instance a3') } }
   }
 })
 
@@ -41,6 +47,18 @@ set(a, {
 
 console.log(a.on.data.fn)
 console.log(a2.on.data.fn)
+console.log(a3.on.data.fn.map(val => val.toString()).join('\n'))
+
+set(a, {
+  on: {
+    data: { GURK: (GURK) => {}, A2: () => { console.log('poooooop') } }
+  }
+}, 'x')
+
+console.log('AFTER')
+console.log(a2.on.data.fn.map(val => val.toString()).join('\n'))
+console.log('--------')
+console.log(a3.on.data.fn.map(val => val.toString()).join('\n'))
 
 // want to remove all instances as well perhaps?
 
