491012042397d04571f6ab6ed795fbee3f01bfb9
Jim de Beer <jim@vigour.io>
2016-10-28 18:55:10 +0200

    instance updates

--- test/listeners.js
+++ test/listeners.js
@@ -3,10 +3,44 @@ const { create, set, get, struct, compute } = require('../')
 
 const a = create(struct, {
   on: {
-    data: { a: () => {} }
+    data: { a: () => { console.log('!!!!') } }
   }
 })
 
-const a2 = create(a, { x: true })
+const b = create(struct, {
+  val: a,
+  on: {
+    data: {
+      1: () => {
+        console.log('1a')
+      }
+    }
+  }
+})
+set(a, null, 'x')
 
-set(a2, null)
+console.log('--------------')
+const a2 = create(a, {
+  on: {
+    data: { b: () => { console.log('my instance a2') } }
+  }
+})
+
+set(a, {
+  on: {
+    data: { xxx: () => { console.log('xxxx') } }
+  }
+}, 'x')
+
+console.log('remove listener?')
+set(a, {
+  on: {
+    data: { a: null }
+  }
+}, 'x')
+
+console.log(a.on.data.fn)
+console.log(a2.on.data.fn)
+
+// want to remove all instances as well perhaps?
+
