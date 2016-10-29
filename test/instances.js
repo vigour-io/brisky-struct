825ab2487fc0882b8441edbe9106f6e1c2f16834
Jim de Beer <jim@vigour.io>
2016-10-29 17:25:13 +0200

    correct keys removal

--- test/instances.js
+++ test/instances.js
@@ -7,13 +7,17 @@ const a = create(struct, {
 })
 
 const b = create(a, { b: true })
-
 const c = create(b)
+const c2 = create(c, { b: null })
 
 const d = create(c, { x: true })
+const e = create(d)
+const f = create(d, { bla: true })
 
 console.log(a.keys)
 console.log(b.keys)
+console.log('c2', c2.keys)
+
 // console.log(c.keys)
 
 console.log('GO GO GO')
@@ -22,4 +26,7 @@ set(a, { b: null, d: true })
 console.log('a', a.keys)
 console.log('b', b.keys) // console.log('b')
 console.log('c', c.keys)
+console.log('c2', c2.keys)
 console.log('d', d.keys)
+console.log('e', e.keys)
+console.log('f', f.keys)
