b9f39f78ea03647cdfc2df2e33d1872b93cfc0d0
Jim de Beer <jim@vigour.io>
2016-10-29 21:43:52 +0200

    replace array with number

--- test/index.js
+++ test/index.js
@@ -1,80 +1,79 @@
-'use strict'
 const { create, set, get, struct, compute } = require('../')
 const bstamp = require('brisky-stamp')
 
-const a = create(struct, {
-  val: 1,
-  b: 2,
-  c: 3,
-  on: {
-    data: {
-      1: (t, val) => {
-        // need to add path + context
-        console.log('fire!', t.key, t.keys)
-      }
-    }
-  }
-})
-a.key = 'a'
+// const a = create(struct, {
+//   val: 1,
+//   b: 2,
+//   c: 3,
+//   on: {
+//     data: {
+//       1: (t, val) => {
+//         // need to add path + context
+//         console.log('fire!', t.key, t.keys)
+//       }
+//     }
+//   }
+// })
+// a.key = 'a'
 
-const a2 = create(a, {
-  c: 4,
-  spesh: true
-})
-a2.key = 'a2'
+// const a2 = create(a, {
+//   c: 4,
+//   spesh: true
+// })
+// a2.key = 'a2'
 
-const a22 = create(a2)
-a22.key = 'a22'
+// const a22 = create(a2)
+// a22.key = 'a22'
 
-const a3 = create(a, {
-  val: 2,
-  yuzi: true
-})
-a3.key = 'a3'
+// const a3 = create(a, {
+//   val: 2,
+//   yuzi: true
+// })
+// a3.key = 'a3'
 
 
-const bla = create(struct, {
-  val: a2,
-  on: {
-    data: {
-      x: () => {
-        console.log('YESSSSS')
-      }
-    }
-  }
-})
+// const bla = create(struct, {
+//   val: a2,
+//   on: {
+//     data: {
+//       x: () => {
+//         console.log('YESSSSS')
+//       }
+//     }
+//   }
+// })
 
-const xxx = create(bla, {})
+// const xxx = create(bla, {})
 
-var s
+// var s
 
-console.log('-------------------------------------------')
-console.log('\n\ngo set case')
+// console.log('-------------------------------------------')
+// console.log('\n\ngo set case')
 
-s = bstamp.create()
-set(a, 3, s)
-bstamp.close(s)
+// s = bstamp.create()
+// set(a, 3, s)
+// bstamp.close(s)
 
-// want to fire for a2
-console.log('a:', compute(a), 'a2:', compute(a2), 'a3:', compute(a3))
+// // want to fire for a2
+// console.log('a:', compute(a), 'a2:', compute(a2), 'a3:', compute(a3))
 
-console.log('-------------------------------------------')
-console.log('\n\ngo multi case')
-s = bstamp.create()
-set(a, { yuzi: true }, s)
-bstamp.close(s)
+// console.log('-------------------------------------------')
+// console.log('\n\ngo multi case')
+// s = bstamp.create()
+// set(a, { yuzi: true }, s)
+// bstamp.close(s)
 
-console.log('-------------------------------------------')
-console.log('\n update exsiting key (remove)')
-s = bstamp.create()
-set(a, { c: null }, s)
-bstamp.close(s)
+// console.log('-------------------------------------------')
+// console.log('\n update exsiting key (remove)')
+// s = bstamp.create()
+// set(a, { c: null }, s)
+// bstamp.close(s)
 
-console.log('-------------------------------------------')
-console.log('\n update exsiting key (spesh)')
-s = bstamp.create()
-set(a, { spesh: true }, s)
-bstamp.close(s)
+// console.log('-------------------------------------------')
+// console.log('\n update exsiting key (spesh)')
+// s = bstamp.create()
+// set(a, { spesh: true }, s)
+// bstamp.close(s)
 
 console.log('-------------------------------------------')
 console.log('\n CONTEXT TIME')
@@ -127,18 +126,20 @@ const x3 = create(struct, {
   }
 })
 
-// const x4 = create(struct, {
-//   A: {
-//     B: {
-//       props: { default: x3 },
-//       C: true
-//     }
-//   }
-// })
+const x4 = create(struct, {
+  A: {
+    B: {
+      props: { default: x3 },
+      C: true
+    }
+  }
+})
 
 // console.log(b.c.contextPath, x.a.contextPath, x.a.b.X.contextPath, b.c.context.key, x.a.b.X.context.key)
 
-// set(get(x4, [ 'A', 'B', 'C', 'xx', 'xxx', 'XXXX', 'a', 'b', 'X', 'c' ]), 'TRIPPLE MOFO')
+console.log(get(x4, [ 'A', 'B', 'C', 'xx', 'xxx', 'XXXX', 'a', 'b', 'X', 'C' ].contextLevel))
+
+// set(get(x4, [ 'A', 'B', 'C', 'xx', 'xxx', 'XXXX', 'a', 'b', 'X', 'C' ]), 'TRIPPLE MOFO')
 
 // console.log(x3.xx.xxx.XXXX)
 // console.log(b.c.val)
@@ -150,12 +151,12 @@ const x3 = create(struct, {
 // const x4 = create(x)
 // const x5 = create(x)
 // const x6 = create(x)
-console.log(' \n\n')
-s = bstamp.create()
-set(b, { C: 'hello' }, s)
-bstamp.close(s)
+// console.log(' \n\n')
+// s = bstamp.create()
+// set(b, { C: 'hello' }, s)
+// bstamp.close(s)
 
 // require('./prop')
 // require('./listeners')
 // require('./instances')
-console.log('!!!!!!!', path(get(x3, [ 'xx', 'xxx', 'XXXX', 'a', 'b', 'X', 'C' ])))
+// console.log('!!!!!!!', path(get(x3, [ 'xx', 'xxx', 'XXXX', 'a', 'b', 'X', 'C' ])))
