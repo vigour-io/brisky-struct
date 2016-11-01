05f472561090f2894e6b64eb15bf633f0ea71fca
Mustafa Dokumaci <mustafa@foraa.com>
2016-11-01 13:51:49 +0100

    test contextLevels

--- test/context/basic.js
+++ test/context/basic.js
@@ -24,8 +24,7 @@ test('context - basic', t => {
   t.equals(compute(get(instance, ['parent', 'child', 'grandChild'])), 'rebel', 'instance has primitive')
   t.equals(compute(get(instance, ['parent', 'child', 'grandChild'])), 'rebel', 'extended has primitive')
   t.notOk(get(instance, ['parent', 'child', 'grandChild', 'age']), 'instance does not have age')
-  t.equals(compute(get(extended, ['parent', 'child', 'grandChild', 'age'])), 17, 'extended has age')
-
+  t.equals(compute(get(extended, ['parent', 'child', 'grandChild', 'age'])), 17, 'age of extended grandChild is 17')
   const grandChild = get(instance, ['parent', 'child', 'grandChild'])
   set(grandChild, { name: 'Berk' })
 
@@ -62,11 +61,18 @@ test('context - props', t => {
     }
   })
 
-  t.equals(compute(get(students, ['first', 'gender'])), 'female', 'Burcu is a female')
-  t.equals(compute(get(students, ['second', 'gender'])), 'male', 'Mert is a male')
+  t.equals(compute(get(students, ['first', 'gender'])), 'female', 'gender of Burcu is female')
+  t.equals(get(students, ['first', 'gender']).context, get(students, 'first'), 'gender should be in context')
+  t.equals(get(students, ['first', 'gender']).contextLevel, 1, 'contextLevel should be 1')
+
+  t.equals(compute(get(students, ['second', 'gender'])), 'male', 'gender of Mert is male')
+  t.equals(get(students, ['second', 'gender']).context, get(students, 'second'), 'gender should be in context')
+  t.equals(get(students, ['second', 'gender']).contextLevel, 1, 'contextLevel should be 1')
 
   set(students, { third: { name: 'Lale', type: 'female' } })
-  t.equals(compute(get(students, ['third', 'gender'])), 'female', 'Lale is a female')
+  t.equals(compute(get(students, ['third', 'gender'])), 'female', 'gender of Lale is female')
+  t.equals(get(students, ['third', 'gender']).context, get(students, 'third'), 'gender should be in context')
+  t.equals(get(students, ['third', 'gender']).contextLevel, 1, 'contextLevel should be 1')
 
   t.end()
 })
