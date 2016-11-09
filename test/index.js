9b0c7010f39de432074bec3b85d82d35fd0b81e2
Jim de Beer <jim@vigour.io>
2016-11-09 16:55:00 +0100

    correct length

--- test/index.js
+++ test/index.js
@@ -18,16 +18,38 @@ bla.set({
   val: 'xxxxx'
 })
 
-bla.set({
-  x: 'XXX'
+const instance = bla.create({
+  x: 'XXX',
+  y: 'yyyy',
+  z: 'zzzz',
+  u: 'xxxx',
+  f: {
+    g: {
+      h: {
+        x: true
+      }
+    }
+  }
 })
 
+for (let i = 0; i < 10; i++) {
+  instance.push(i)
+}
+
+console.log(instance.f.g.h.x)
+
+// console.log(instance.map((val, key, keys) => val.compute()))
+
+// console.log(instance.filter(val => val.compute() === 'XXX'))
+console.log(instance.x)
+
+console.log(instance)
 // parent is #1
 // maybe key as well (make it like _K and _P)
 // can add extend again -- not a biggy
 
-const x = bla.create()
-console.log(bla.x.hello(), bla.x.compute())
+const x = instance.create()
+console.log(instance.x.hello(), instance.x.compute())
 console.log('??', x.compute())
 // console.log(bla)
 // require('./context')
