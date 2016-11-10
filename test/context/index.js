b2a70a536190f07acd27a19913c08da06884e814
Jim de Beer <jim@vigour.io>
2016-11-10 13:19:15 +0100

    context tests, adhere to new api

--- test/context/index.js
+++ test/context/index.js
@@ -1,3 +1,3 @@
 'use strict'
 require('./basic')
-// require('./nested')
+require('./nested')
--- test/context/nested.js
+++ test/context/nested.js
@@ -1,28 +1,27 @@
 'use strict'
-
 const test = require('tape')
-const { create, set, get, struct, compute, path, parent } = require('../../')
+const struct = require('../../')
 
 test('context - nested', t => {
-  const feline = create(struct, {
+  const feline = struct({
     flies: false,
     runs: true,
     swims: false
   })
 
-  const cat = create(feline)
+  const cat = feline.create()
 
   var slow = {}
 
-  const bird = create(struct, {
+  const bird = struct({
     flies: true,
     runs: {
       val: false,
       on: {
         data: {
           slow (t) {
-            const p = path(t).join('.')
-            slow[p] = slow[p] || { val: compute(t), count: 0 }
+            const p = t.path().join('.')
+            slow[p] = slow[p] || { val: t.compute(), count: 0 }
             slow[p].count++
           }
         }
@@ -32,25 +31,23 @@ test('context - nested', t => {
   })
   bird._k = 'bird'
 
-  const pigeon = create(bird)
-  pigeon._k = 'pigeon'
+  const pigeon = bird.create()
+  pigeon._k = 'pigeon' // maybe make a key property
 
-  const seagull = create(bird, {
+  const seagull = bird.create({
     speed: 11,
     swims: true,
     likes: {
       props: { woundedPigeon: pigeon },
-      woundedPigeon: {
-        flies: false,
-      }
+      woundedPigeon: { flies: false }
     }
   })
   seagull._k = 'seagull'
 
-  const animals = create(struct, { types: { pigeon, seagull, cat } })
-  animals._k = 'animals' // add key as property?
+  const animals = struct({ types: { pigeon, seagull, cat } })
+  animals._k = 'animals'
 
-  set(animals, {
+  animals.set({
     mySeagull: {
       type: 'seagull',
       runs: true,
@@ -63,45 +60,86 @@ test('context - nested', t => {
     }
   })
 
-  t.equals(compute(get(animals, ['mySeagull', 'speed'])), 11, 'my seagull has 11 speed')
-  t.ok(compute(get(animals, ['mySeagull', 'flies'])), 'my seagull flies')
-  t.equals(get(animals, ['mySeagull', 'flies']).context, get(animals, 'mySeagull'), 'my seagull flies in context')
-  t.equals(get(animals, ['mySeagull', 'flies']).contextLevel, 1, 'contextLevel is 1')
-  t.ok(compute(get(animals, ['mySeagull', 'swims'])), 'my seagull swims')
-  t.equals(get(animals, ['mySeagull', 'swims']).context, get(animals, 'mySeagull'), 'my seagull swims in context')
-  t.equals(get(animals, ['mySeagull', 'swims']).contextLevel, 1, 'contextLevel is 1')
-  t.ok(compute(get(animals, ['mySeagull', 'runs'])), 'my seagull runs')
-  t.notOk(get(animals, ['mySeagull', 'runs']).context, 'my seagull runs out of context')
-  t.equals(compute(get(animals, ['mySeagull', 'hunts', 'kitten', 'runs'])), false, 'my seagull hunts kitten which can not run')
-  t.notOk(get(animals, ['mySeagull', 'hunts', 'kitten', 'runs']).context, 'hunted kitten can not run out of context')
-  t.equals(get(animals, ['mySeagull', 'hunts', 'kitten', 'swims']).context, get(animals, ['mySeagull', 'hunts', 'kitten']), 'hunted kitten can not swim in context')
-  t.equals(get(animals, ['mySeagull', 'hunts', 'kitten', 'swims']).contextLevel, 1, 'contextLevel is 1')
-  t.equals(compute(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'flies'])), false, 'my seagull likes wounded pigeon which can not fly')
-  t.equals(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'swims']).context, get(animals, ['mySeagull', 'likes', 'woundedPigeon']), 'wounded pigeon can not swim in context')
-  t.equals(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'swims']).contextLevel, 1, 'contextLevel is 1')
-  t.equals(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'swims']).context.context, get(animals, ['mySeagull']), 'wounded pigeon can not swim in context of context')
-  t.equals(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'swims']).context.contextLevel, 2, 'contextLevel of context is 2')
-  t.equals(parent(get(animals, ['mySeagull', 'hunts', 'kitten', 'swims'])), get(animals, ['mySeagull', 'hunts', 'kitten']), 'parent of swims is kitten')
-  t.equals(parent(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'runs'])), get(animals, ['mySeagull', 'likes', 'woundedPigeon']), 'parent of runs is woundedPigeon')
+  t.equals(
+    animals.get(['mySeagull', 'speed']).compute(), 11,
+    'my seagull has 11 speed'
+  )
 
-  slow = {}
+  t.ok(animals.get(['mySeagull', 'flies']).compute(), 'my seagull flies')
 
-  console.log(' \n OK HERE--------------------')
-  set(bird, { runs: { slow: true } }, 'stamp1')
-  t.deepEqual(slow, {
-    'bird.runs': { val: false, count: 1 },
-    'seagull.runs': { val: false, count: 1 },
-    'pigeon.runs': { val: false, count: 1 },
-    // why does this fire twice? -- once for pigeon and once for bird
-    'animals.mySeagull.likes.woundedPigeon.runs': { val: false, count: 1 },
-    'animals.mySeagull.runs': { val: true, count: 1 }
-  }, 'first slow event fired as expected')
+  t.equals(
+    animals.get(['mySeagull', 'flies']).context, animals.get('mySeagull'),
+    'my seagull flies in context'
+  )
 
-  slow = {}
-  set(animals, { mySeagull: { likes: { woundedPigeon: { runs: { slow: false } } } } }, 'stamp2')
-  t.deepEqual(slow, {
-    'animals.mySeagull.likes.woundedPigeon.runs': { val: false, count: 1 },
-  }, 'second slow event fired as expected')
+  t.equals(
+    animals.get(['mySeagull', 'flies']).contextLevel, 1,
+    'contextLevel is 1'
+  )
+
+  t.ok(animals.get(['mySeagull', 'swims']).compute(), 'my seagull swims')
+
+  t.equals(
+    animals.get(['mySeagull', 'swims']).context, animals.get('mySeagull'),
+    'my seagull swims in context'
+  )
+
+  t.equals(
+    animals.get(['mySeagull', 'swims']).contextLevel, 1,
+    'contextLevel is 1'
+  )
+
+  t.ok(animals.get(['mySeagull', 'runs']).compute(), 'my seagull runs')
+
+  t.notOk(animals.get(['mySeagull', 'runs']).context, 'my seagull runs out of context')
+
+  t.equals(
+    animals.get(['mySeagull', 'hunts', 'kitten', 'runs']).compute(), false,
+    'my seagull hunts kitten which can not run'
+  )
+
+  t.notOk(
+    animals.get(['mySeagull', 'hunts', 'kitten', 'runs']).context,
+    'hunted kitten can not run out of context'
+  )
+
+  t.equals(
+    animals.get(['mySeagull', 'hunts', 'kitten', 'swims']).context,
+    animals.get(['mySeagull', 'hunts', 'kitten']),
+    'hunted kitten can not swim in context'
+  )
+
+  t.equals(
+    animals.get(['mySeagull', 'hunts', 'kitten', 'swims']).contextLevel, 1,
+    'contextLevel is 1'
+  )
+
+  // t.equals(compute(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'flies'])), false, 'my seagull likes wounded pigeon which can not fly')
+  // t.equals(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'swims']).context, get(animals, ['mySeagull', 'likes', 'woundedPigeon']), 'wounded pigeon can not swim in context')
+  // t.equals(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'swims']).contextLevel, 1, 'contextLevel is 1')
+  // t.equals(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'swims']).context.context, get(animals, ['mySeagull']), 'wounded pigeon can not swim in context of context')
+  // t.equals(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'swims']).context.contextLevel, 2, 'contextLevel of context is 2')
+  // t.equals(parent(get(animals, ['mySeagull', 'hunts', 'kitten', 'swims'])), get(animals, ['mySeagull', 'hunts', 'kitten']), 'parent of swims is kitten')
+  // t.equals(parent(get(animals, ['mySeagull', 'likes', 'woundedPigeon', 'runs'])), get(animals, ['mySeagull', 'likes', 'woundedPigeon']), 'parent of runs is woundedPigeon')
+
+  // slow = {}
+
+  // console.log(' \n OK HERE--------------------')
+  // set(bird, { runs: { slow: true } }, 'stamp1')
+  // t.deepEqual(slow, {
+  //   'bird.runs': { val: false, count: 1 },
+  //   'seagull.runs': { val: false, count: 1 },
+  //   'pigeon.runs': { val: false, count: 1 },
+  //   // why does this fire twice? -- once for pigeon and once for bird
+  //   'animals.mySeagull.likes.woundedPigeon.runs': { val: false, count: 1 },
+  //   'animals.mySeagull.runs': { val: true, count: 1 }
+  // }, 'first slow event fired as expected')
+
+  // slow = {}
+  // set(animals, { mySeagull: { likes: { woundedPigeon: { runs: { slow: false } } } } }, 'stamp2')
+  // t.deepEqual(slow, {
+  //   'animals.mySeagull.likes.woundedPigeon.runs': { val: false, count: 1 },
+  // }, 'second slow event fired as expected')
 
   t.end()
 })
