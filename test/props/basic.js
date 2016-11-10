a7dd61e99779e3cd4ca38d866df092de4f6bed61
Jim de Beer <jim@vigour.io>
2016-11-10 12:02:58 +0100

    refactor tests to use methods

--- test/props/basic.js
+++ test/props/basic.js
@@ -1,65 +1,61 @@
 'use strict'
 const test = require('tape')
-const { create, set, get, struct, compute } = require('../../')
+const struct = require('../../')
 
 test('props - default', t => {
-  const s = create(struct, {
+  const s = struct({
     props: {
       something: true
     },
     something: 'wrong'
   })
-
-  t.equal(get(s, 'something'), 'wrong', 'something is wrong')
+  t.equal(s.get('something'), 'wrong', 'something is wrong')
   t.end()
 })
 
 test('props - function', t => {
-  const s = create(struct, {
+  const s = struct({
     props: {
       speed (s, val) {
-        set(s, { fast: val > 50 })
+        s.set({ fast: val > 50 })
       }
     },
     speed: 40
   })
-
-  t.equal(compute(get(s, 'fast')), false, 'not fast yet')
-
-  set(s, { speed: 60 })
-
-  t.equal(compute(get(s, 'fast')), true, 'now fast')
+  t.equal(s.get('fast').compute(), false, 'not fast yet')
+  s.set({ speed: 60 })
+  t.equal(s.get('fast').compute(), true, 'now fast')
   t.end()
 })
 
 test('props - nested', t => {
-  const s = create(struct, {
+  const s = struct({
     props: {
       _parent: {
         child: 'rebel'
       }
     }
   })
-  set(s, { _parent: 'conservative' })
-  t.equal(compute(get(s, '_parent')), 'conservative', '_parent is conservative')
-  t.equal(compute(get(s, ['_parent', 'child'])), 'rebel', 'child is rebel')
+  s.set({ _parent: 'conservative' })
+  t.equal(s.get('_parent').compute(), 'conservative', '_parent is conservative')
+  t.equal(s.get(['_parent', 'child']).compute(), 'rebel', 'child is rebel')
   t.end()
 })
 
 test('props - default', t => {
-  const s = create(struct, {
+  const s = struct({
     props: {
       default: {
         props: {
           _volume (s, val) {
-            set(s, {
+            s.set({
               volume: val,
-              mass: compute(get(s, 'density')) * val
+              mass: s.get('density').compute() * val
             })
           },
           _mass (s, val) {
-            set(s, {
-              volume: val / compute(get(s, 'density')),
+            s.set({
+              volume: val / s.get('density').compute(),
               mass: val
             })
           }
@@ -76,13 +72,13 @@ test('props - default', t => {
     }
   })
 
-  t.equal(compute(get(s, ['water', 'density'])), 1, 'density of water is 1')
-  set(s, { gold: { density: 19.3, melting: 1064.18 } })
-  t.equal(compute(get(s, ['gold', 'density'])), 19.3, 'density of gold is 19.3')
-  set(s, { water: { _volume: 10 } })
-  set(s, { gold: { _mass: 193 } })
-  t.equal(compute(get(s, ['water', 'mass'])), 10, 'mass of water is 10')
-  t.equal(compute(get(s, ['gold', 'volume'])), 10, 'volume of gold is 10')
+  t.equal(s.get(['water', 'density']).compute(), 1, 'density of water is 1')
+  s.set({ gold: { density: 19.3, melting: 1064.18 } })
+  t.equal(s.get(['gold', 'density']).compute(), 19.3, 'density of gold is 19.3')
+  s.set({ water: { _volume: 10 } })
+  s.set({ gold: { _mass: 193 } })
+  t.equal(s.get(['water', 'mass']).compute(), 10, 'mass of water is 10')
+  t.equal(s.get(['gold', 'volume']).compute(), 10, 'volume of gold is 10')
 
   t.end()
 })
