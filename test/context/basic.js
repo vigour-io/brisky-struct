897467976ffd322433ce7438683af9e2bde80f9f
Jim de Beer <jim@vigour.io>
2016-11-10 13:01:50 +0100

    added basic context tests

--- test/context/basic.js
+++ test/context/basic.js
@@ -25,14 +25,17 @@ test('context - basic', t => {
     instance.get(['_parent', 'child', 'grandChild']).compute(), 'rebel',
     'instance has primitive'
   )
+
   t.equals(
     instance.get(['_parent', 'child', 'grandChild']).compute(), 'rebel',
     'extended has primitive'
   )
+
   t.notOk(
     instance.get(['_parent', 'child', 'grandChild', 'age']),
     'instance does not have age'
   )
+
   t.equals(
     extended.get(['_parent', 'child', 'grandChild', 'age']).compute(), 17,
     'age of extended grandChild is 17'
@@ -45,66 +48,106 @@ test('context - basic', t => {
     instance.get(['_parent', 'child', 'grandChild', 'name']).compute(), 'Berk',
     'instance has name'
   )
+
   t.equals(
     grandChild.get('name').compute(), 'Berk',
     'assigned sub-struct has name'
   )
+
   t.equals(
     extended.get(['_parent', 'child', 'grandChild', 'name']).compute(), 'Berk',
     'extended has name'
   )
 
   t.equals(
-    grandChild.parent(), instance.get(['_parent', 'child']),
-    '_parent of grandChild is child'
+    grandChild.get('parent'), instance.get(['_parent', 'child']),
+    'parent of grandChild is child'
   )
 
   t.equals(
     extended.get(['_parent', 'child', 'grandChild', 'parent']), extended.get(['_parent', 'child']),
-    'extended child is _parent of extended grandChild'
+    'extended child is parent of extended grandChild'
   )
 
   t.end()
 })
 
-// test('context - props', t => {
-//   const female = create(struct, {
-//     gender: 'female'
-//   })
+test('context - props', t => {
+  const female = struct({ gender: 'female' })
 
-//   const male = create(struct, {
-//     gender: 'male'
-//   })
+  const male = struct({ gender: 'male' })
 
-//   const students = create(struct, {
-//     props: {
-//       default: female
-//     },
-//     types: { female, male },
-//     first: {
-//       name: 'Burcu'
-//     },
-//     second: {
-//       name: 'Mert',
-//       type: 'male'
-//     }
-//   })
+  const students = struct({
+    props: { default: female },
+    types: { female, male },
+    first: { name: 'Burcu' },
+    second: {
+      name: 'Mert',
+      type: 'male'
+    }
+  })
 
-//   t.equals(compute(get(students, ['first', 'gender']).compute(), 'female', 'gender of Burcu is female')
-//   t.equals(get(students, ['first', 'gender']).context, get(students, 'first'), 'gender is in context')
-//   t.equals(get(students, ['first', 'gender']).contextLevel, 1, 'contextLevel is 1')
-//   t.equals(parent(get(students, ['first', 'gender'])), get(students, 'first'), '_parent of gender for Burcu is first')
+  t.equals(
+    students.get(['first', 'gender']).compute(), 'female',
+    'gender of Burcu is female'
+  )
 
-//   t.equals(compute(get(students, ['second', 'gender']).compute(), 'male', 'gender of Mert is male')
-//   t.equals(get(students, ['second', 'gender']).context, get(students, 'second'), 'gender is in context')
-//   t.equals(get(students, ['second', 'gender']).contextLevel, 1, 'contextLevel is 1')
-//   t.equals(parent(get(students, ['second', 'gender'])), get(students, 'second'), '_parent of gender for Mert is second')
+  t.equals(
+    students.get(['first', 'gender']).context, students.get('first'),
+    'gender is in context'
+  )
 
-//   set(students, { third: { name: 'Lale', type: 'female' } })
-//   t.equals(compute(get(students, ['third', 'gender'])), 'female', 'gender of Lale is female')
-//   t.equals(get(students, ['third', 'gender']).context, get(students, 'third'), 'gender is in context')
-//   t.equals(get(students, ['third', 'gender']).contextLevel, 1, 'contextLevel is 1')
-//   t.equals(parent(get(students, ['third', 'gender'])), get(students, 'third'), '_parent of gender for Lale is third')
+  t.equals(
+    students.get(['first', 'gender']).contextLevel, 1,
+    'contextLevel is 1'
+  )
 
-//   t.end()
-// })
+  t.equals(
+    students.get(['first', 'gender', 'parent']), students.get('first'),
+    'parent of gender for Burcu is first'
+  )
+
+  t.equals(
+    students.get(['second', 'gender']).compute(), 'male',
+    'gender of Mert is male'
+  )
+
+  t.equals(
+    students.get(['second', 'gender']).context, students.get('second'),
+    'gender is in context'
+  )
+
+  t.equals(
+    students.get(['second', 'gender']).contextLevel, 1,
+    'contextLevel is 1'
+  )
+
+  t.equals(
+    students.get(['second', 'gender', 'parent']), students.get('second'),
+    'parent of gender for Mert is second'
+  )
+
+  students.set({ third: { name: 'Lale', type: 'female' } })
+
+  t.equals(
+    students.get(['third', 'gender']).compute(), 'female',
+    'gender of Lale is female'
+  )
+
+  t.equals(
+    students.get(['third', 'gender']).context, students.get('third'),
+    'gender is in context'
+  )
+
+  t.equals(
+    students.get(['third', 'gender']).contextLevel, 1,
+    'contextLevel is 1'
+  )
+
+  t.equals(
+    students.get(['third', 'gender', 'parent']), students.get('third'),
+    'parent of gender for Lale is third'
+  )
+
+  t.end()
+})
