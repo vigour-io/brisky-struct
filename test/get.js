22c59f4fb437e7259a49b132267e6832fdc52a2f
Jim de Beer <jim@vigour.io>
2016-11-13 12:48:43 +0100

    remove get by index

--- test/get.js
+++ test/get.js
@@ -8,3 +8,9 @@ test('get - defaults', t => {
   t.equal(a.get([ 'a', 'b', 'd' ], 0).val, 0, 'array - falsy')
   t.end()
 })
+
+test('get - methods', t => {
+  const a = struct({ a: { b: { c: true } } })
+  t.equal(a.a.b.c.get('parent'), a.a.b, 'parent')
+  t.end()
+})
