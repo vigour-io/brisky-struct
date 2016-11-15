f82e39cdf42fbad9a2e47831698409fa7f6cffda
Jim de Beer <jim@vigour.io>
2016-11-15 16:18:29 +0100

    100% coverage for keys

--- test/iterators.js
+++ test/iterators.js
@@ -8,6 +8,12 @@ test('iterators - keys', t => {
   a.set({ a: null, b: null })
   t.same(a.keys(), [ 'c' ], 'correct keys on "a" after remove')
   t.same(b.keys(), [ 'a', 'c', 'd' ], 'correct keys on "b" after remove')
+  b.get('c').set(null)
+  t.same(b.keys(), [ 'a', 'd' ], 'correct keys on "b" after context remove')
+  t.same(a.keys(), [ 'c' ], 'did not influence "a"')
+  const c = b.create()
+  c.get('d').set(null)
+  t.same(c.keys(), [ 'a' ], 'correct keys on "c" after context remove')
   t.end()
 })
 
