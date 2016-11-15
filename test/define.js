20d0500abd796644a891c78bdfb2fc64a7245ef8
Jim de Beer <jim@vigour.io>
2016-11-15 16:10:04 +0100

    added tests for define

---
+++ test/define.js
@@ -0,0 +1,29 @@
+const test = require('tape')
+const struct = require('../')
+
+test('define ', t => {
+  var cnt = 0
+  const a = struct({
+    define: {
+      hello () {
+        return ++cnt
+      }
+    }
+  })
+
+  const b = a.create({
+    define: {
+      hello () {
+        return --cnt
+      }
+    }
+  })
+
+  t.not(b.Constructor, a.Constructor, 'create new constructor for b')
+
+  t.equal(a.hello(), 1, 'defined hello on a')
+
+  t.equal(b.hello(), 0, 'defined hello on b')
+
+  t.end()
+})
