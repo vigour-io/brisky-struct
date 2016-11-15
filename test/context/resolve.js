28b1d8d572ebd30ad0fa4ff6671a95de55a1fd80
Jim de Beer <jim@vigour.io>
2016-11-15 11:56:09 +0100

    added remove tests, context remove returns null in set

--- test/context/resolve.js
+++ test/context/resolve.js
@@ -7,6 +7,7 @@ test('context - resolve', t => {
     key: 'a',
     b: {
       c: {
+        d: {},
         on: {
           data: {
             results: (t) => {
@@ -24,17 +25,26 @@ test('context - resolve', t => {
         default: a
       },
       z: {},
-      z2: {}
+      z2: {},
+      z3: {}
     }
   })
-  const z = x.get([ 'y', 'z', 'b', 'c' ])
-  const resolved = z.set('haha', 'stamp')
-  t.not(resolved, z, 'resolved context')
+  const zC = x.get([ 'y', 'z', 'b', 'c' ])
+  const resolved = zC.set('haha', 'stamp')
+  t.not(resolved, zC, 'resolved context (level 2)')
   t.same(results, [ [ 'x', 'y', 'z', 'b', 'c' ] ], 'fired correct listeners')
-  t.equal(z.context, null, 'cleared context on resolve')
-  t.equal(z.contextLevel, null, 'cleared contextLevel on resolve')
-  const z2 = x.get([ 'y', 'z2', 'b' ])
-  const resolved2 = z2.set('haha', 'stamp')
-  t.not(resolved2, z2, 'resolved context - low level')
+  t.equal(zC.context, null, 'cleared context on resolve')
+  t.equal(zC.contextLevel, null, 'cleared contextLevel on resolve')
+
+  const z2B = x.get([ 'y', 'z2', 'b' ])
+  const resolved2 = z2B.set('haha', 'stamp')
+  t.not(resolved2, z2B, 'resolved context (level 1)')
+
+  const z3D = x.get([ 'y', 'z3', 'b', 'c', 'd' ])
+  const resolved3 = z3D.set(null, 'stamp')
+  t.equal(resolved3, null, 'returns null from context remove')
+  t.not(resolved3, z3D, 'resolved context (level 3)')
+  t.equal(x.get([ 'y', 'z3', 'b', 'c', 'd' ]), void 0, 'removed d')
+
   t.end()
 })
