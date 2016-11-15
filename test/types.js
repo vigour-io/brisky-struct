9ff6d3931c6590a467a9476bf5e4e8d7f307f5f8
Jim de Beer <jim@vigour.io>
2016-11-15 14:08:12 +0100

    type tests

--- test/types.js
+++ test/types.js
@@ -7,9 +7,31 @@ test('types ', t => {
     types: {
       something: {
         field: 'real'
-      }
+      },
+      bla: { somehting: 'wrong' }
+    },
+    field: { type: 'something' }
+  })
+
+  const b = a.create({
+    types: {
+      something: {
+        type: 'something',
+        bla: true
+      },
+      bla: 'override!'
+    },
+    x: {
+      type: 'something'
+    },
+    y: {
+      type: 'bla'
     }
   })
 
+  t.same(b.get('x').keys(), [ 'field', 'bla' ], 'merged "something" type')
+  t.equal(b.get('y').keys(), void 0, 'override "bla" type')
+  t.equal(b.get('y').compute(), 'override!', 'type with string')
+  t.same(a.get('field').keys(), [ 'field' ], '"field" on a has "field"')
   t.end()
 })
