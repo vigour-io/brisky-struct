8b171fc9f1bc975cef846a66edffb1bcc838ef6a
Jim de Beer <jim@vigour.io>
2016-11-16 12:12:19 +0100

    formatting

--- test/iterators.js
+++ test/iterators.js
@@ -6,13 +6,9 @@ test('iterators', t => {
   t.same(a.keys(), [ 'a', 'b', 'c' ], 'correct initial keys')
   const b = a.create({ key: 2, d: true, a: true })
   t.same(a.keys(), [ 'a', 'b', 'c' ], 'correct keys after creating an instance')
-
-  console.log('go for it')
   a.set({ a: null, b: null })
   t.same(a.keys(), [ 'c' ], 'correct keys on "a" after remove')
-  console.log(b._ks, b.keys())
   t.same(b.keys(), [ 'c', 'd' ], 'correct keys on "b" after remove')
-  console.log(b.keys()) // only c and d
   b.get('c').set(null)
   t.same(b.keys(), [ 'd' ], 'correct keys on "b" after context remove')
   t.same(a.keys(), [ 'c' ], 'did not influence "a"')
@@ -26,9 +22,7 @@ test('iterators', t => {
   t.same(d.filter(val => val.key === 'a'), [ d.a ], 'filter')
   t.equal(d.reduce((a, b) => a + b.compute(), ''), 'abc', 'reduce')
   const result = []
-  d.forEach(val => {
-    result.push(val)
-  })
+  d.forEach(val => result.push(val))
   t.same(d.map(val => val), [ d.a, d.b, d.c ], 'forEach')
   t.end()
 })
