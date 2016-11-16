4eeacc5849fa726c3f7273fd7174d07ac4165479
Jim de Beer <jim@vigour.io>
2016-11-16 12:57:31 +0100

    faster removal

--- test/remove.js
+++ test/remove.js
@@ -25,31 +25,36 @@ test('remove ', t => {
     },
     on: t => results.push(t.path())
   })
-
   const s2 = s.create({ key: 's2' }) //eslint-disable-line
-
-  /*
-  [ [ 's2' ],
-  [ 's2', 'bla' ],
-  [ 's2', 'bla', 'a', 'b', 'c' ],
-  [ 's2', 'x', 'z' ],
-  [ 's' ],
-  [ 's', 'bla' ],
-  [ 's', 'bla', 'a', 'b', 'c' ],
-  [ 's', 'x', 'z' ] ]
-  */
-
   const obj = struct({ //eslint-disable-line
     key: 'obj',
     props: {
       default: s
     },
-    hello: {}
+    hello: {},
+    blurf: {
+      x: {
+        z: 'haha'
+      }
+    }
   })
-
   s.set(null, 'stamp')
-
-  console.log(results)
-
+  t.same(results, [
+    [ 'obj', 'blurf' ],
+    [ 'obj', 'blurf', 'bla' ],
+    [ 'obj', 'blurf', 'bla', 'a', 'b', 'c' ],
+    [ 'obj', 'blurf', 'x', 'z' ],
+    [ 'obj', 'hello' ],
+    [ 'obj', 'hello', 'bla' ],
+    [ 'obj', 'hello', 'bla', 'a', 'b', 'c' ],
+    [ 'obj', 'hello', 'x', 'z' ],
+    [ 's2' ],
+    [ 's2', 'bla' ],
+    [ 's2', 'bla', 'a', 'b', 'c' ],
+    [ 's2', 'x', 'z' ],
+    [ 's' ],
+    [ 's', 'bla' ],
+    [ 's', 'bla', 'a', 'b', 'c' ]
+  ], 'fires all listeners on remove')
   t.end()
 })
