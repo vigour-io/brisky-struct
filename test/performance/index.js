4772d32243db5af762ad567e8b4998d0011b1feb
Jim de Beer <jim@vigour.io>
2016-11-17 12:32:10 +0100

    add parent stamp

--- test/performance/index.js
+++ test/performance/index.js
@@ -1,6 +1,7 @@
 const perf = require('brisky-performance')
 const struct = require('../../')
-const Obs = require('vigour-observable')
+// const Obs = require('vigour-observable')
+const State = require('vigour-state')
 const n = 1e4
 const bs = require('brisky-stamp')
 
@@ -86,17 +87,17 @@ const bs = require('brisky-stamp')
 //   }
 // }, 'create listeners')
 
-perf(() => {
-  const a = struct({ on: { data: { log: () => {} } } })
-  for (let i = 0; i < n * 100; i++) {
-    a.set(i, bs.create())
-  }
-}, () => {
-  const a = new Obs({ on: { data: { log: () => {} } } })
-  for (let i = 0; i < n * 100; i++) {
-    a.set(i)
-  }
-}, `fire listeners n = ${(n * 100 / 1e3) | 0}k`)
+// perf(() => {
+//   const a = struct({ on: { data: { log: () => {} } } })
+//   for (let i = 0; i < n * 100; i++) {
+//     a.set(i, bs.create())
+//   }
+// }, () => {
+//   const a = new Obs({ on: { data: { log: () => {} } } })
+//   for (let i = 0; i < n * 100; i++) {
+//     a.set(i)
+//   }
+// }, `fire listeners n = ${(n * 100 / 1e3) | 0}k`)
 
 // perf(() => {
 //   const orig = struct({
@@ -124,28 +125,54 @@ perf(() => {
 //   }
 // }, 'create references')
 
+// perf(() => {
+//   const a = struct()
+//   struct({
+//     val: a,
+//     on: {
+//       data: { lol () {} }
+//     }
+//   })
+//   for (let i = 0; i < n * 100; i++) {
+//     a.set(i, bs.create())
+//   }
+// }, () => {
+//   const a = new Obs()
+//   new Obs({ // eslint-disable-line
+//     val: a,
+//     on: {
+//       data: {
+//         lol () {}
+//       }
+//     }
+//   })
+//   for (let i = 0; i < n * 100; i++) {
+//     a.set(i)
+//   }
+// }, `fire listeners over references n = ${(n * 100 / 1e3) | 0}k`)
+
 perf(() => {
-  const a = struct()
-  struct({
-    val: a,
-    on: {
-      data: { lol () {} }
-    }
-  })
+  const a = struct({})
   for (let i = 0; i < n * 100; i++) {
     a.set(i, bs.create())
   }
 }, () => {
-  const a = new Obs()
-  new Obs({ // eslint-disable-line
-    val: a,
-    on: {
-      data: {
-        lol () {}
-      }
-    }
-  })
+  const a = new State({})
   for (let i = 0; i < n * 100; i++) {
     a.set(i)
   }
-}, `fire listeners over references n = ${(n * 100 / 1e3) | 0}k`)
+}, `fire listeners vs vigour-state n = ${(n * 100 / 1e3) | 0}k`)
+
+perf(() => {
+  const s = struct({ a: { b: { c: {} } } })
+  const a = s.a.b.c
+  for (let i = 0; i < n * 100; i++) {
+    a.set(i, bs.create())
+  }
+}, () => {
+  const s = new State({ a: { b: { c: {} } } })
+  const a = s.a.b.c
+  for (let i = 0; i < n * 100; i++) {
+    a.set(i)
+  }
+}, `fire listeners vs vigour-state deep n = ${(n * 100 / 1e3) | 0}k`)
