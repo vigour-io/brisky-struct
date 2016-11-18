a4472aeaf893c1610dd17a9bbc63dd048ff5cbe4
Jim de Beer <jim@vigour.io>
2016-11-18 11:43:09 +0100

    ovveride test

--- test/compute.js
+++ test/compute.js
@@ -4,15 +4,20 @@ const struct = require('../')
 test('compute ', t => {
   const a = struct({
     props: {
-      default: { $transform: (val) => val + '!' }
+      default: { $transform: val => val + '!' }
     },
     field: 'wow',
     hello: {
       val: 'hello',
       $transform: null
+    },
+    bye: {
+      val: 'bye',
+      $transform: val => val + '?'
     }
   })
   t.equal(a.get('field').compute(), 'wow!', 'inherits $transform')
   t.equal(a.get('hello').compute(), 'hello', 'remove transform')
+  t.equal(a.get('bye').compute(), 'bye?', 'override transform')
   t.end()
 })
