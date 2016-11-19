0a0c1ec51c71211ddb52008d6aa3d6e264d39781
Jim de Beer <jim@vigour.io>
2016-11-19 17:18:24 +0100

    added simple remove, all basic any tests pass

--- test/subscribe/any/basic.js
+++ test/subscribe/any/basic.js
@@ -122,135 +122,138 @@ test('subscription - any - basic - true', t => {
   t.end()
 })
 
-// test('subscription - any - basic - val: "property"', t => {
-//   var s = subsTest(
-//     t,
-//     {},
-//     { $any: { val: 'property' } }
-//   )
+test('subscription - any - basic - val: "property"', t => {
+  var s = subsTest(
+    t,
+    {},
+    { $any: { val: 'property' } }
+  )
 
-//   s('initial subscription', [], {})
+  s('initial subscription', [], {})
 
-//   s(
-//     'create fields',
-//     [
-//       { path: 'a', type: 'new' },
-//       { path: 'b', type: 'new' }
-//     ],
-//     {
-//       a: {},
-//       b: {}
-//     }
-//   )
+  s(
+    'create fields',
+    [
+      { path: 'a', type: 'new' },
+      { path: 'b', type: 'new' }
+    ],
+    {
+      a: {},
+      b: {}
+    }
+  )
 
-//   s(
-//     'set fields',
-//     [],
-//     {
-//       a: 'a',
-//       b: 'b'
-//     }
-//   )
+  s(
+    'set fields',
+    [],
+    {
+      a: 'a',
+      b: 'b'
+    }
+  )
 
-//   s(
-//     'remove field',
-//     [
-//       { path: 'a', type: 'remove' }
-//     ],
-//     { a: null }
-//   )
+  s(
+    'remove field',
+    [
+      { path: 'a', type: 'remove' }
+    ],
+    { a: null }
+  )
 
-//   t.end()
-// })
+  t.end()
+})
 
-// test('subscription - any - basic - combined with a field with nested subs', t => {
-//   var s = subsTest(
-//     t,
-//     {},
-//     {
-//       field: { nested: { val: true } },
-//       $any: { val: true }
-//     }
-//   )
+test('subscription - any - basic - combined with a field with nested subs', t => {
+  var s = subsTest(
+    t,
+    {},
+    {
+      field: { nested: { val: true } },
+      $any: { val: true }
+    }
+  )
 
-//   s('initial subscription', [], {})
+  s('initial subscription', [], {})
 
-//   s(
-//     'create fields',
-//     [
-//       { path: 'field/nested', type: 'new' },
-//       { path: 'a', type: 'new' },
-//       { path: 'field', type: 'new' }
-//     ],
-//     {
-//       a: {},
-//       field: {
-//         nested: 'hello'
-//       }
-//     }
-//   )
+  s(
+    'create fields',
+    [
+      { path: 'field/nested', type: 'new' },
+      { path: 'a', type: 'new' },
+      { path: 'field', type: 'new' }
+    ],
+    {
+      a: {},
+      field: {
+        nested: 'hello'
+      }
+    }
+  )
 
-//   t.end()
-// })
+  t.end()
+})
 
-// test('subscription - any - basic - empty fields', t => {
-//   var s = subsTest(
-//     t,
-//     {
-//       fields: [ true, true ]
-//     },
-//     {
-//       fields: {
-//         $remove: true,
-//         $any: { val: true, $remove: true }
-//       }
-//     }
-//   )
+test('subscription - any - basic - empty fields', t => {
+  var s = subsTest(
+    t,
+    {
+      fields: [ true, true ]
+    },
+    {
+      fields: {
+        $remove: true,
+        $any: { val: true, $remove: true }
+      }
+    }
+  )
 
-//   s('initial subscription', [
-//     { path: 'fields/0', type: 'new' },
-//     { path: 'fields/1', type: 'new' }
-//   ])
+  s('initial subscription', [
+    { path: 'fields/0', type: 'new' },
+    { path: 'fields/1', type: 'new' }
+  ])
 
-//   s(
-//     'remove fields',
-//     [ { type: 'remove' }, { type: 'remove' } ],
-//     {
-//       fields: { reset: true }
-//     }
-//   )
+  s(
+    'remove fields',
+    [
+      { path: 'fields/0', type: 'remove' },
+      { path: 'fields/1', type: 'remove' }
+    ],
+    {
+      fields: { 0: null, 1: null }
+    }
+  )
 
-//   t.end()
-// })
+  t.end()
+})
 
-// test('subscription - any - basic - remove nested fields using $remove listener', t => {
-//   var s = subsTest(
-//     t,
-//     {
-//       fields: [ true, true ]
-//     },
-//     {
-//       fields: {
-//         $remove: true,
-//         $any: { val: true, $remove: true }
-//       }
-//     }
-//   )
+test('subscription - any - basic - remove nested fields using $remove listener', t => {
+  var s = subsTest(
+    t,
+    {
+      fields: [ true, true ]
+    },
+    {
+      fields: {
+        $remove: true,
+        $any: { val: true, $remove: true }
+      }
+    }
+  )
 
-//   s('initial subscription', [
-//     { path: 'fields/0', type: 'new' },
-//     { path: 'fields/1', type: 'new' }
-//   ])
+  s('initial subscription', [
+    { path: 'fields/0', type: 'new' },
+    { path: 'fields/1', type: 'new' }
+  ])
 
-//   s(
-//     'remove fields',
-//     [
-//       { path: 'fields/0', type: 'remove' }, { path: 'fields/1', type: 'remove' }
-//     ],
-//     {
-//       fields: null
-//     }
-//   )
+  s(
+    'remove fields',
+    [
+      { path: 'fields/0', type: 'remove' }, { path: 'fields/1', type: 'remove' }
+    ],
+    {
+      fields: null
+    }
+  )
 
-//   t.end()
-// })
+  t.end()
+})
