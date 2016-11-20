7216379b41cd9697a72ac8e0f6f62d2efa275e71
Jim de Beer <jim@vigour.io>
2016-11-20 23:46:01 +0100

    merge

--- test/subscribe/any/merge.js
+++ test/subscribe/any/merge.js
@@ -27,16 +27,41 @@ test('subscription - any - merge', t => {
     ]
   )
 
-// [ { path: 'a/x', type: 'remove' }, { path: 'b/y', type: 'remove' }, { path: 'c/z', type: 'remove' }
+  /*
+    { path: 'a/x', type: 'remove' },
+    { path: 'b/y', type: 'remove' },
+    { path: 'c/z', type: 'remove' },
+    { path: 'b/y', type: 'new' },
+    { path: 'c/x', type: 'new' },
+    { path: 'c/z', type: 'new' }
+  */
 
   s(
     'change reference to b',
     [
-      // { path: 'a/x', type: 'remove' },
-      // { path: 'c/x', type: 'new' },
-      // { path: 'c/z', type: 'new' }
+     { path: 'a/x', type: 'remove' }, { path: 'c/x', type: 'new' }
     ],
     { collection: [ '@', 'parent', 'b' ] }
+  )
+
+  s(
+    'change reference to c',
+    [
+     { path: 'b/y', type: 'remove' },
+     { path: 'c/y', type: 'new' }
+    ],
+    { collection: [ '@', 'parent', 'c' ] }
+  )
+
+  s(
+    'change reference to a',
+    [
+     { path: 'c/x', type: 'remove' },
+     { path: 'c/y', type: 'remove' },
+     { path: 'a/x', type: 'new' },
+     { path: 'b/y', type: 'new' }
+    ],
+    { collection: [ '@', 'parent', 'a' ] }
   )
 
   t.end()
