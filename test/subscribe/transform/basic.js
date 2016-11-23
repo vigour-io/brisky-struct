cde58e3fcca65dbfbb6bcb252a2d3377163b7b71
Jim de Beer <jim@vigour.io>
2016-11-23 12:33:54 +0100

    transform drivers

--- test/subscribe/transform/basic.js
+++ test/subscribe/transform/basic.js
@@ -4,29 +4,42 @@ const struct = require('../../../')
 
 test('subscription - $transform - basic', t => {
   const s = struct({
-    a: {
-      x: 'hello'
-    },
-    b: {
-      c: 'bye'
+    collection: {
+      a: {
+        x: 'hello'
+      },
+      b: {
+        c: 'bye'
+      }
     },
     qeury: 'hello'
   })
 
-  s.subscribe({
-    $any: {
-      // root: {
-      //   qeury: {} // wtf??? why this this fire ?????
-      // },
-      $transform: (t, subs, tree) => {
-        return { x: { val: true } } // if parse parse results of functions // bit of a waste but fuck it -- make faster later
+  const tree = s.subscribe({
+    collection: {
+      $any: {
+        // this has to work differently in the transform unforutately
+        $transform: (t, subs, tree) => {
+          return { x: { val: true } } // if parse parse results of functions // bit of a waste but fuck it -- make faster later
+        },
+        $transform2: {
+          val: (t, subs, tree) => {
+            console.log('blurrrr')
+            return { c: { val: true } } // if parse parse results of functions // bit of a waste but fuck it -- make faster later
+          },
+          root: {
+            qeury: { val: true }
+          }
+        }
       }
     }
   }, (t) => {
-    console.log('go go go', t.path())
-    console.log('lulllz')
+    console.log('FIRE:', t.path())
   })
 
+  console.log(tree.collection.$any.b.$c)
+
+  console.log(' \nextra!')
   s.qeury.set('yo qeury')
 
   t.end()
