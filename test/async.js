402620ac5421557484761b06892e0c6e066f2386
Jim de Beer <jim@vigour.io>
2016-12-05 19:32:51 +0100

    renamed defer to timeout

--- test/async.js
+++ test/async.js
@@ -3,7 +3,7 @@ const struct = require('../')
 const stamp = require('brisky-stamp')
 
 test('async', t => {
-  const defer = (val, time = 5, err) =>
+  const timeout = (val, time = 5, err) =>
     new Promise((resolve, reject) =>
       setTimeout(() => err ? reject(err) : resolve(val), time)
     )
@@ -29,54 +29,54 @@ test('async', t => {
 
   // before coveralls support async/await need a solution for this
   // const later = async val => {
-  //   val = await defer(val, 10) + '!'
+  //   val = await timeout(val, 10) + '!'
   //   return val
   // }
 
-  a.set(defer('later-1'), s)
+  a.set(timeout('later-1'), s)
 
-  a.set(defer('defer-1'), s)
+  a.set(timeout('timeout-1'), s)
 
-  a.set(defer('defer-2'), s)
+  a.set(timeout('timeout-2'), s)
 
   a.set(function* (t, stamp) {
     for (var i = 0; i < 3; i++) {
-      yield defer('await-gen-' + i)
+      yield timeout('await-gen-' + i)
     }
   }, s)
 
-  a.set(defer('defer-error', 0, new Error('haha')), s)
+  a.set(timeout('timeout-error', 0, new Error('haha')), s)
 
   a.set(function* logGenerator () {
     for (var i = 0; i < 3; i++) {
-      yield defer('gen-' + i, 10)
+      yield timeout('gen-' + i, 10)
     }
   }, s)
 
-  a.set(defer({ val: 'defer-3', bla: { bla: defer() } }, 25), s)
+  a.set(timeout({ val: 'timeout-3', bla: { bla: timeout() } }, 25), s)
 
-  a.set(defer('defer-4'), s)
+  a.set(timeout('timeout-4'), s)
 
-  a.set(defer('defer-5'), s)
+  a.set(timeout('timeout-5'), s)
 
-  a.set(a.once('gen-1').then(() => defer('defer-6', 25)), s)
+  a.set(a.once('gen-1').then(() => timeout('timeout-6', 25)), s)
 
-  a.set({ val: defer('defer-7'), hello: true }, s)
+  a.set({ val: timeout('timeout-7'), hello: true }, s)
 
-  a.once('defer-7').then(() => {
-    t.pass('defer-7 is set')
+  a.once('timeout-7').then(() => {
+    t.pass('timeout-7 is set')
     const s = stamp.create('move')
     a.set(function* logGenerator () {
       for (var i = 0; i < 3; i++) {
-        yield defer('gen-2-' + i, 100)
+        yield timeout('gen-2-' + i, 100)
       }
     }, s)
-    a.set(defer('defer-8', 1e3), s)
-    a.set(defer('defer-9', 1e3), s)
-    a.set(defer('defer-10', 1e3), s)
-    a.set(defer('defer-11', 1e3), s)
-    a.set({ async: null, val: defer('defer-12') }, s)
-    a.once('defer-12', () => {
+    a.set(timeout('timeout-8', 1e3), s)
+    a.set(timeout('timeout-9', 1e3), s)
+    a.set(timeout('timeout-10', 1e3), s)
+    a.set(timeout('timeout-11', 1e3), s)
+    a.set({ async: null, val: timeout('timeout-12') }, s)
+    a.once('timeout-12', () => {
       t.same(errors, [ 'haha' ], 'fired correct errors')
       t.same(results, [
         `click-${scnt + 1}`,
@@ -97,7 +97,7 @@ test('async', t => {
         `move-${scnt + 17}`
       ], 'fired correct results')
 
-      a.set(defer('no stamp!'), false)
+      a.set(timeout('no stamp!'), false)
       setTimeout(() => {
         t.equal(a.compute(), 'no stamp!', 'async input without stamp')
         const s = stamp.create('special')
@@ -118,24 +118,24 @@ test('async', t => {
           var i = 3
           while (i--) {
             if (i === 2) {
-              yield defer('ha!')
+              yield timeout('ha!')
             }
             yield 'gen-' + i
           }
         }, s)
 
         const arr = [
-          defer('iterator-1'),
+          timeout('iterator-1'),
           'iterator-2',
-          defer('iterator-3')
+          timeout('iterator-3')
         ]
 
         a.set({ val: arr[Symbol.iterator]() }, s)
 
         const arr2 = [
-          defer('iterator-1-1'),
+          timeout('iterator-1-1'),
           'iterator-2-2',
-          defer('iterator-3-3')
+          timeout('iterator-3-3')
         ]
 
         a.set(arr2[Symbol.iterator](), s)
