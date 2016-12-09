c27bbf9a7583a7c458d2881339eb5b3e908dda06
Jim de Beer <jim@vigour.io>
2016-12-09 13:38:13 +0100

    fixed reference problems

--- test/subscribe/switch/references.js
+++ test/subscribe/switch/references.js
@@ -1,5 +1,6 @@
 const test = require('tape')
 const subsTest = require('../util')
+const struct = require('../../../')
 
 test('subscription - $switch - references', t => {
   const s = subsTest(t, {
@@ -22,5 +23,58 @@ test('subscription - $switch - references', t => {
     { path: 'b/a', type: 'new' }
   ], { field: [ '@', 'root', 'b' ] })
 
+  t.end()
+})
+
+test('subscription - $switch - references', t => {
+  const state = struct()
+  var results = []
+  var cnt = 0
+
+  state.subscribe({
+    field: {
+      navigation: {
+        $switch (val) {
+          cnt++
+          return {
+            title: { val: true },
+            navigation: {
+              $switch (val) {
+                return {
+                  title: { val: true }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }, (s, type) => results.push(s.path(), type))
+
+  state.set({
+    otheritems: {
+      first: { title: 'first' },
+      second: { rating: 100 }
+    },
+    items: {
+      first: {
+        title: 'first',
+        navigation: {}
+      },
+      second: { rating: 100 }
+    },
+    field: {
+      val: 'blurf',
+      navigation: {
+        val: [ '@', 'root', 'items', 'first' ]
+      }
+    }
+  })
+
+  t.same([ [ 'items', 'first', 'title' ], 'new' ], results, 'results')
+  t.equal(cnt, 1, 'top switch fired once')
+  results = []
+  state.items.first.navigation.set([ '@', 'root', 'items', 'first' ])
+  t.same([ [ 'items', 'first', 'title' ], 'new' ], results, 'results')
   t.end()
 })
