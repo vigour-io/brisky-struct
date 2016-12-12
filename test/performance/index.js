98006d8308c84e0063a749cc9e52840a65c08506
Jim de Beer <jim@vigour.io>
2016-12-12 22:24:01 +0100

    re-enabled perf

--- test/performance/index.js
+++ test/performance/index.js
@@ -226,76 +226,76 @@ var n = 1e3 // eslint-disable-line
 //   }
 // }, `creation n = ${(n * 10 / 1e3) | 0}k`, 10)
 
-// var s = struct({})
-// var arr = []
-// for (let i = 0; i < 3e5; i++) {
-//   arr.push(i)
-// }
-// s.subscribe(
-//   { $any: { val: true } },
-//   () => {}
-// )
-// // will also optmize creation!
-// s.set(arr)
+var s = struct({})
+var arr = []
+for (let i = 0; i < 3e5; i++) {
+  arr.push(i)
+}
+s.subscribe(
+  { $any: { val: true } },
+  () => {}
+)
+// will also optmize creation!
+s.set(arr)
 
-// var ss = new State({})
-// var arrs = []
-// for (let i = 0; i < 3e5; i++) {
-//   arrs.push(i)
-// }
-// ss.subscribe(
-//   { $any: { val: true } },
-//   () => {}
-// )
-// // will also optmize creation!
-// ss.set(arrs)
+var ss = new State({})
+var arrs = []
+for (let i = 0; i < 3e5; i++) {
+  arrs.push(i)
+}
+ss.subscribe(
+  { $any: { val: true } },
+  () => {}
+)
+// will also optmize creation!
+ss.set(arrs)
 
-// perf(() => {
-//   s[(Math.random() * 3e5) | 0].set('face')
-// }, () => {
-//   ss[(Math.random() * 3e5) | 0].set('face')
-// }, `any subscription large set n = 300k`)
+perf(() => {
+  s[(Math.random() * 3e5) | 0].set('face')
+}, () => {
+  ss[(Math.random() * 3e5) | 0].set('face')
+}, `any subscription large set n = 300k`)
 
-// perf(() => {
-//   for (let i = 0; i < n * 10; i++) {
-//     let s = struct({})
-//     s.subscribe(
-//       { $any: { val: true } },
-//       () => {},
-//       true
-//     )
-//     s.set({ [i]: i })
-//   }
-// }, () => {
-//   for (let i = 0; i < n * 10; i++) {
-//     let s = new State({})
-//     s.subscribe(
-//       { $any: { val: true } },
-//       () => {}
-//     )
-//     s.set({ [i]: i })
-//   }
-// }, `any subscription creation n = ${(n * 10 / 1e3) | 0}k`)
+perf(() => {
+  for (let i = 0; i < n * 10; i++) {
+    let s = struct({})
+    s.subscribe(
+      { $any: { val: true } },
+      () => {},
+      true
+    )
+    s.set({ [i]: i })
+  }
+}, () => {
+  for (let i = 0; i < n * 10; i++) {
+    let s = new State({})
+    s.subscribe(
+      { $any: { val: true } },
+      () => {}
+    )
+    s.set({ [i]: i })
+  }
+}, `any subscription creation n = ${(n * 10 / 1e3) | 0}k`)
 
-// perf(() => {
-//   const s = struct({})
-//   s.subscribe(
-//     { $any: { val: true } },
-//     () => {}
-//   )
-//   for (let i = 0; i < n; i++) {
-//     s.set({ [i]: i })
-//   }
-// }, () => {
-//   const s = new State({})
-//   s.subscribe(
-//     { $any: { val: true } },
-//     () => {}
-//   )
-//   for (let i = 0; i < n; i++) {
-//     s.set({ [i]: i })
-//   }
-// }, `any subscription n = ${((n / 1e3) | 0)}k`)
+perf(() => {
+  const s = struct({})
+  s.subscribe(
+    { $any: { val: true } },
+    () => {}
+  )
+  for (let i = 0; i < n; i++) {
+    s.set({ [i]: i })
+  }
+}, () => {
+  const s = new State({})
+  s.subscribe(
+    { $any: { val: true } },
+    () => {}
+  )
+  for (let i = 0; i < n; i++) {
+    s.set({ [i]: i })
+  }
+}, `any subscription n = ${((n / 1e3) | 0)}k`)
 
 // perf(() => {
 //   const s = struct({ a: { b: { c: {} } } })
