6305dbb6e58e851ca5f56ee311e4f27c0186b31d
Jim de Beer <jim@vigour.io>
2016-12-14 15:43:11 +0100

    fixes

--- test/subscribe/any/filter.js
+++ test/subscribe/any/filter.js
@@ -1,23 +1,35 @@
 const test = require('tape')
-const subsTest = require('../util')
+const struct = require('../../../')
 
-// add simple array methods for collection -- where you handle the keys
-// also lets handle resort a bit later (by using indexes in the tree as keys)
 test('subscription - any - filter', t => {
-  subsTest(
-    t,
-    {
-      collection: {
-        a: true,
-        b: true,
-        c: true
-      }
-    },
-    {
-      collection: {
-        $any: { val: true }
+  var results = []
+  const s = struct({
+    collection: {
+      a: true,
+      b: true,
+      c: true
+    }
+  })
+
+  s.subscribe({
+    collection: {
+      $any: {
+        $keys: keys => keys.concat().reverse(), // needs to be immtuable else issues
+        val: true
       }
     }
-  )
+  }, (val, type) => {
+    console.log(val.key, type)
+    results.push(val.key)
+  })
+  t.same(results, [ 'c', 'b', 'a' ], 'initial subscription')
+
+  results = []
+  s.set({ collection: { x: true } })
+  t.same(results, [ 'x', 'c', 'b', 'a' ], 'initial subscription')
+
+  // console.log(results)
   t.end()
 })
+
+// do one with switch and root etc
