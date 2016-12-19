0e35c16e54e7975776efe1c2c1117baf9c733eac
Jim de Beer <jim@vigour.io>
2016-12-19 17:20:38 +0100

    tests pass

--- test/subscribe/index.js
+++ test/subscribe/index.js
@@ -1,7 +1,8 @@
-// require('./method')
-// require('./references')
-// require('./composite')
-// require('./switch')
-// require('./context')
+require('./method')
+require('./references')
+require('./composite')
+require('./switch')
 require('./basic')
 require('./any')
+// some things for hub are going to be tricky here
+require('./context')
