1d4e12f61e8b53047960060466553e2d94286e80
Jim de Beer <jim@vigour.io>
2016-12-22 20:25:38 +0100

    add travis

--- rollup/dist.js
+++ rollup/dist.js
@@ -1,5 +1,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import sourcemaps from 'rollup-plugin-sourcemaps'
 
 export default {
   entry: 'lib/index.js',
@@ -7,14 +8,15 @@ export default {
     nodeResolve({
       jsnext: true
     }),
-    commonjs()
+    commonjs(),
+    sourcemaps()
   ],
   sourceMap: true,
-  external: [
-    'brisky-stamp'
-  ],
+  external: [ 'brisky-stamp' ],
   targets: [
     { dest: 'dist/index.js', format: 'cjs' },
     { dest: 'dist/index.es.js', format: 'es' }
   ]
 }
+
+// import sourcemaps from 'rollup-plugin-sourcemaps';
