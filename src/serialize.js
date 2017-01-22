c5e03aac5b5613044de2a9ed1c120457b9fe917f
Youri Daamen <me@youz.io>
2017-01-23 00:39:45 +0100

    make arrays in serialize

--- src/serialize.js
+++ src/serialize.js
@@ -3,7 +3,6 @@ import { getKeys } from './keys'
 import { path } from './traversal'
 
 const getVal = t => t.val !== void 0 ? t.val : t.inherits && getVal(t.inherits)
-
 const serialize = (t, fn) => {
   var result = {}
   var val = getVal(t)
@@ -15,12 +14,20 @@ const serialize = (t, fn) => {
     while (i--) { val[i + 2] = p[i] }
   }
   if (keys) {
+    let onlyNumbers = true
     for (let i = 0, len = keys.length; i < len; i++) {
       let key = keys[i]
       let keyResult = serialize(get(t, key), fn)
+      if (isNaN(key)) onlyNumbers = false
       if (keyResult !== void 0) { result[key] = keyResult }
     }
-    if (val !== void 0) { result.val = val }
+    if (val !== void 0) {
+      result.val = val
+    } else if (onlyNumbers) {
+      const arr = []
+      for (let i in result) arr[i] = result[i]
+      result = arr
+    }
   } else if (val !== void 0) {
     result = val
   }
