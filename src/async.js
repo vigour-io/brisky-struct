259cf0f139925d89b586d1e17ba5db67938b1343
Jim de Beer <jim@vigour.io>
2017-01-30 20:32:19 +0100

    handle context for async sets

--- src/async.js
+++ src/async.js
@@ -2,12 +2,13 @@ import bs from 'brisky-stamp'
 import { generic } from './emit'
 import { root } from './traversal'
 import { set } from './manipulate'
-import { storeContext, applyContext } from './context'
 
 var uid = 0
 const extendSet = (t, val, stamp, context) => {
-  // console.log(context)
-  applyContext(t, context)
+  if (t._c) {
+    t._c = null
+    t._cLevel = null
+  }
   if (stamp) {
     set(t, val, bs.create())
     bs.close()
@@ -34,7 +35,7 @@ const generator = (t, val, stamp) => iterator(t, val(t, stamp), stamp)
 
 const promiseQueue = (t, uid, val, error) => {
   if (t.async) {
-    for (let i = 0, end = t.async.length - 3; i < end; i += 4) {
+    for (let i = 0, end = t.async.length - 2; i < end; i += 3) {
       if (t.async[i + 2] === uid) {
         t.async[i] = val
         t.async[i + 2] = void 0
@@ -46,7 +47,7 @@ const promiseQueue = (t, uid, val, error) => {
 }
 
 const done = t => {
-  t.async.splice(0, 4)
+  t.async.splice(0, 3)
   if (t.async.length) { queue(t) }
   if (t.async && !t.async.length) {
     delete t.async
@@ -67,10 +68,10 @@ const execPromise = t => {
   if (async !== void 0) {
     if (Array.isArray(async)) {
       for (let i = 0, len = async.length; i < len; i++) {
-        extendSet(t, async[i], t.async[1], t.async[3])
+        extendSet(t, async[i], t.async[1])
       }
     } else {
-      extendSet(t, async, t.async[1], t.async[3])
+      extendSet(t, async, t.async[1])
     }
   }
   done(t)
@@ -97,7 +98,7 @@ const execIterator = (t, iteratee, stamp, id, done, val) => {
           val.value
           .then(resolved => {
             if (t.async && t.async[2] === id) {
-              extendSet(t, resolved, stamp, t.async[3])
+              extendSet(t, resolved, stamp)
               execIterator(t, iteratee, stamp, id, done, next(iteratee, t, stamp))
             }
           })
@@ -108,7 +109,7 @@ const execIterator = (t, iteratee, stamp, id, done, val) => {
             }
           })
         } else {
-          extendSet(t, val.value, stamp, t.async[3])
+          extendSet(t, val.value, stamp)
           execIterator(t, iteratee, stamp, id, done, next(iteratee, t, stamp))
         }
       } else {
@@ -124,20 +125,20 @@ const iterator = (t, iteratee, stamp, val) => {
   const id = ++uid
   if (!t.async) {
     // add 4th context
-    t.async = [ iteratee, stamp, id, storeContext(t) ]
+    t.async = [ iteratee, stamp, id ]
     queue(t)
   } else {
-    t.async.push(iteratee, stamp, id, storeContext(t))
+    t.async.push(iteratee, stamp, id)
   }
 }
 
 const promise = (t, promise, stamp) => {
   const id = ++uid
   if (!t.async) {
-    t.async = [ void 0, stamp, id, storeContext(t) ]
+    t.async = [ void 0, stamp, id ]
     queue(t)
   } else {
-    t.async.push(void 0, stamp, id, storeContext(t))
+    t.async.push(void 0, stamp, id)
   }
   promise
     .then(val => promiseQueue(t, id, val))
--- test/async/context.js
+++ test/async/context.js
@@ -7,7 +7,7 @@ const timeout = (val, time = 5, err) =>
     setTimeout(() => err ? reject(err) : resolve(val), time)
   )
 
-test('async - context', t => {
+test('async - context - clear', t => {
   const a = create({
     b: {
       c: 'hello'
@@ -18,6 +18,22 @@ test('async - context', t => {
   a1.get([ 'b', 'c' ])
   setTimeout(() => {
     t.equal(a.b.c.val, 'bye')
+    t.end()
+  }, 100)
+})
+
+test('async - context - restore', t => {
+  const a = create({
+    b: {
+      c: 'hello'
+    }
+  })
+  const a1 = a.create()
+  a1.get([ 'b', 'c' ]).set(timeout('lulz'))
+  a1.get([ 'b', 'c' ])
+  setTimeout(() => {
+    t.equal(a.b.c.val, 'hello')
+    t.equal(a1.b.c.val, 'lulz')
     t.end()
   }, 100)
 })
