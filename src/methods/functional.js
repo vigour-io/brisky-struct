b0e76b0fe62e1238868aaa3f3a9960e7bbfc281f
Jim de Beer <jim@vigour.io>
2017-02-27 13:34:39 +0100

    added all functional keys

--- src/methods/functional.js
+++ src/methods/functional.js
@@ -1,31 +1,90 @@
 import { get } from '../get'
 import { getKeys } from '../keys'
-// add every, find, sort, slice
+
+const mapper = (t) => (getKeys(t) || []).map(key => get(t, key))
 
 export default {
-  reduce (fn, start) {
-    return (getKeys(this) || []).map(key => get(this, key)).reduce(fn, start)
-  },
   map (fn, callee) {
     return (getKeys(this) || []).map((val, key, array) => fn(get(this, val), key, array))
   },
+  reduce (fn, start) {
+    return mapper(this).reduce(fn, start)
+  },
   filter (fn) {
-    return (getKeys(this) || []).map(key => get(this, key)).filter(fn)
+    return mapper(this).filter(fn)
+  },
+  slice (start, end) {
+    return mapper(this).slice(start, end)
+  },
+  sort (fn) {
+    return mapper(this).sort(fn)
+  },
+  reverse (fn) {
+    return mapper(this).reverse(fn)
   },
   find (val) {
-    var keys = getKeys(this)
+    const keys = getKeys(this)
     if (keys) {
       for (let i = 0, len = keys.length; i < len; i++) {
-        let r = get(this, keys[i])
+        const r = get(this, keys[i])
         if (val(r, i, this)) return r
       }
     }
   },
+  some (val) {
+    const keys = getKeys(this)
+    if (keys) {
+      for (let i = 0, len = keys.length; i < len; i++) {
+        const r = get(this, keys[i])
+        if (val(r, i, this)) return true
+      }
+    }
+    return false
+  },
+  every (val) {
+    const keys = getKeys(this)
+    if (keys) {
+      for (let i = 0, len = keys.length; i < len; i++) {
+        if (!val(get(this, keys[i]), i, this)) return false
+      }
+    }
+    return true
+  },
+  findIndex (val) {
+    const keys = getKeys(this)
+    if (keys) {
+      for (let i = 0, len = keys.length; i < len; i++) {
+        if (val(get(this, keys[i]), i, this)) return i
+      }
+    }
+    return -1
+  },
+  indexOf (val) {
+    const keys = getKeys(this)
+    if (keys) {
+      for (let i = 0, len = keys.length; i < len; i++) {
+        const r = get(this, keys[i])
+        if (r.compute() === val || r === val) return i
+      }
+    }
+    return -1
+  },
+  lastIndexOf (val) {
+    const keys = getKeys(this)
+    if (keys) {
+      let i = keys.length
+      while (i--) {
+        const r = get(this, keys[i])
+        if (r.compute() === val || r === val) return i
+      }
+    }
+    return -1
+  },
   includes (val, index = 0) {
-    var keys = getKeys(this)
+    const keys = getKeys(this)
     if (keys) {
       for (let len = keys.length, i = index > -1 ? index : Math.max(len + index, 0); i < len; i++) {
-        let r = get(this, keys[i])
+        const r = get(this, keys[i])
         if (r.compute() === val || r === val) return true
       }
     }
@@ -36,8 +95,8 @@ export default {
     if (keys) {
       keys = keys.concat()  // bit slow but usefull for remove for example
       for (let i = 0, len = keys.length; i < len; i++) {
-        let key = keys[i]
-        let r = get(this, key)
+        const key = keys[i]
+        const r = get(this, key)
         if (r) { fn(r, key, this) }
       }
     }
