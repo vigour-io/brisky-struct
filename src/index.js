651806c404891c09080d856c9087f6718d85a66d
Jim de Beer <jim@vigour.io>
2017-04-01 10:27:43 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import { cuid, uid, puid } from './uid'
 import getType from './struct/types/get'
 import { switchInheritance } from './inheritance'
 
-if (typeof __filename !== 'undefined') console.log('brisky-struct:', __filename)
 const emitterProperty = struct.props.on.struct.props.default
 
 set(struct, { inject: methods })
