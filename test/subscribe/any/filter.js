70b2549434ea64084cdeef9ee5e73e3ea52eb19b
Jim de Beer <jim@vigour.io>
2017-04-11 10:06:33 +0200

    added multi any test case

--- test/subscribe/any/filter.js
+++ test/subscribe/any/filter.js
@@ -112,3 +112,93 @@ test('subscription - any - filter - root', t => {
 
   t.end()
 })
+
+test('subscription - any - multiple filter', t => {
+  const movie1 = { title: 'movie1' }
+  const movie2 = { title: 'movie1' }
+  const show1 = { title: 'show1' }
+  const show2 = { title: 'show2' }
+
+  const s = struct({
+    page: {
+      movie1,
+      movie2,
+      show1,
+      show2,
+      shows: {
+        title: 'shows',
+        items: [
+          [ '@', 'root', 'page', 'show1' ],
+          [ '@', 'root', 'page', 'show2' ]
+        ]
+      },
+      movies: {
+        title: 'movies',
+        items: [
+          [ '@', 'root', 'page', 'movie1' ],
+          [ '@', 'root', 'page', 'movie2' ]
+        ]
+      },
+      search: {
+        shows: {
+          order: 0,
+          val: [ '@', 'root', 'page', 'shows' ]
+        },
+        movies: {
+          order: 1,
+          val: [ '@', 'root', 'page', 'movies' ]
+        }
+      }
+    },
+    search: {}
+  })
+
+  const sInstance = s.create()
+
+  const path = []
+
+  sInstance.subscribe({
+    page: {
+      current: {
+        $any: {
+          items: {
+            $any: {
+              $keys: {
+                root: { search: { query: true } },
+                title: true,
+                val: (keys, s) => keys.filter(key => {
+                  const q = s.root().get([ 'search', 'query', 'compute' ])
+                  if (q && (s.get([ key, 'title', 'compute' ]) || '').indexOf(q) !== -1) {
+                    return true
+                  }
+                })
+              },
+              val: true
+            }
+          }
+        }
+      }
+    }
+  }, (val, type) => {
+    path.push(val.path())
+  })
+
+  sInstance.set({
+    page: {
+      current: [ '@', 'root', 'page', 'search' ]
+    }
+  })
+
+  sInstance.set({
+    search: { query: 'show' }
+  })
+
+  t.same(path, [
+    [ 'search', 'query' ],
+    [ 'page', 'shows', 'items', '0' ],
+    [ 'page', 'shows', 'items', '1' ],
+    [ 'search', 'query' ]
+  ])
+
+  t.end()
+})
