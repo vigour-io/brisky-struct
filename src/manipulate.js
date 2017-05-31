0ffcb0d8f5d4237d38e8d8df7ea71cade478f148
Mustafa Dokumaci <mustafa@foraa.com>
2017-05-31 16:50:26 +0200

    bring back conflict resolution

--- src/manipulate.js
+++ src/manipulate.js
@@ -117,7 +117,7 @@ const overrideObjects = (t, val, stamp, isNew, reset) => {
 
   if (override && t.stamp && !reset) {
     if (Math.abs(t.stamp) > Math.abs(override)) {
-      // return false
+      return false
     }
   }
 
