e1cd1ae7f2eba938a965fbb8cb47c4b4fde6f458
Jim de Beer <jim@vigour.io>
2017-07-30 10:08:21 +0200

    add emit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import struct from './struct'
 import { create as c, set } from './manipulate'
 import methods from './methods'
+import { generic as emit } from './emit'
 import { get, getOrigin, getVal } from './get'
 import { getRefVal } from './references'
 import { getProp as getProperty, property } from './property'
@@ -14,7 +15,7 @@ import getType from './struct/types/get'
 import { switchInheritance } from './inheritance'
 
 const emitterProperty = struct.props.on.struct.props.default
-
+console.log('????B-STRUCT')
 set(struct, { inject: methods })
 
 const create = (val, stamp, t = struct, parent, key) =>
@@ -25,6 +26,7 @@ export {
   parse,
   create,
   compute,
+  emit,
   set,
   struct,
   property,
