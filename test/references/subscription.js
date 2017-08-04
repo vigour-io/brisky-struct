f18f46e5071d2309c50521701774cffec484ca27
Mustafa Dokumaci <mustafa@foraa.com>
2017-08-04 13:59:52 +0200

    improve test

--- test/references/subscription.js
+++ test/references/subscription.js
@@ -110,7 +110,7 @@ test('references - field subscription', t => {
 })
 
 test('references - field subscription local', t => {
-  t.plan(1)
+  t.plan(2)
 
   const master = struct({
     key: 'master',
@@ -124,7 +124,8 @@ test('references - field subscription local', t => {
     },
     otherDeep: {
       pointer1: ['@', 'root', 'deep', 'real'],
-      pointer2: ['@', 'parent', 'pointer1']
+      pointer2: ['@', 'parent', 'pointer1'],
+      pointer3: ['@', 'parent', 'pointer2']
     }
   })
 
@@ -133,8 +134,13 @@ test('references - field subscription local', t => {
   branch.subscribe({ otherDeep: { pointer3: true } }, (val, type) => {
     if (type === 'new') {
       t.equals(
+        val.get(['field', 'compute']), 'is a thing',
+        'pointer3 fired for the original'
+      )
+    } else if (type === 'update') {
+      t.equals(
         val.get(['field', 'compute']), 'is other override',
-        'pointer2 fired for other override'
+        'pointer3 fired for other override'
       )
     }
   })
@@ -147,8 +153,7 @@ test('references - field subscription local', t => {
       }
     },
     otherDeep: {
-      pointer1: ['@', 'root', 'deep', 'otherReal'],
-      pointer3: ['@', 'parent', 'pointer2']
+      pointer1: ['@', 'root', 'deep', 'otherReal']
     }
   })
 })
