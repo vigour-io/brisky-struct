85e9b53617e72bef87e8a369a94174a8dde48ef3
Mustafa Dokumaci <mustafa@foraa.com>
2017-08-16 12:36:02 +0200

    fix puid collusion

--- src/uid.js
+++ src/uid.js
@@ -1,17 +1,23 @@
-import hash from 'string-hash'
 var cnt = 1e4 // so now a limition becomes 10k fns normal
 const uid = t => { return t._uid || (t._uid = ++cnt) }
 const cuid = t => {
   if (t._c) {
     var id = 5381
     while (t) {
-      id = id * 33 ^ uid(t)
+      id = (id * 33) ^ uid(t)
       t = t._cLevel === 1 ? t._c : t._p
     }
     return id >>> 0
   } else {
     return uid(t) - 1e4
   }
+}
+const hash = (id, str) => {
+  var i = str.length
+  while (i) {
+    id = (id * 33) ^ str.charCodeAt(--i)
+  }
+  return id
 }
 const puid = t => {
   var id = 5381
@@ -20,7 +26,7 @@ const puid = t => {
     while (p) {
       let key = p.key
       if (key !== void 0) {
-        id = id * 33 ^ (hash(key))
+        id = hash(id, key)
         p = p._cLevel === 1 ? p._c : p._p
       } else {
         return id >>> 0
@@ -33,7 +39,7 @@ const puid = t => {
     while (p) {
       let key = p.key
       if (key !== void 0) {
-        id = id * 33 ^ (hash(key))
+        id = hash(id, key)
         p = p._p
       } else {
         return (t._puid = id >>> 0)
